7425678b9f50c17240e533c0bef73dbbe748583a
Shubhadeep Mahato <shubhadeepmahato123@gmail.com>
2023-05-08 10:50:46 +0530

    payment page form optimized

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -14,21 +14,29 @@ export default function Payment() {
     const [paymentDone, setPaymentDone] = useState(true)
     const [showOverlay, setShowOverlay] = useState(false);
     const [orderDone, setOrderDone] = useState(false);
+    const [userDetails,setUserDetails]=useState({
+        name:"",
+        phone:"",
+        address:"",
+        paymentMethod:""
+
+    })
     let navigate=useNavigate()
     let [loading, setLoading] = useState(true);
     // let [color, setColor] = useState("#ffffff");
     const handleSubmit = (event) => {
         event.preventDefault();
         setShowOverlay(true)
+        setTimeout(() => {
+            setLoading(false)
+            setOrderDone(true)
+        }, 3000);
 
     }
     
     
      
-    setTimeout(() => {
-        setLoading(false)
-        setOrderDone(true)
-    }, 3000);
+  
    
    
 
@@ -41,7 +49,7 @@ export default function Payment() {
                 {showOverlay && (
                     <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex items-center justify-center">
                         <div className="bg-white rounded-lg p-10">
-                            <h2 className="text-lg font-bold mb-4">Order Confirmed</h2>
+                         {orderDone && <h2 className="text-lg font-bold mb-4">Order Confirmed</h2>}
                             <ClipLoader
                                 color='blue'
                                 loading={loading}
@@ -59,7 +67,7 @@ export default function Payment() {
             {/* Order Details */}
             <div className="z-0 bg-gray-200 flex justify-center max-w-2xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
                 <div className=" ">
-                    <form className="bg-white p-6 rounded-lg shadow-md">
+                    <form className="bg-white p-6 rounded-lg shadow-md" onSubmit={handleSubmit}>
                         <div className="mb-4">
                             <label className="block text-gray-700 font-bold mb-2" for="name">Name:</label>
                             <input required className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" id="name" name="name" />
@@ -71,8 +79,8 @@ export default function Payment() {
                         </div>
 
                         <div className="mb-4">
-                            <label required className="block text-gray-700 font-bold mb-2" for="address">Address:</label>
-                            <textarea className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="address" name="address"></textarea>
+                            <label  className="block text-gray-700 font-bold mb-2" for="address">Address:</label>
+                            <textarea required className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="address" name="address"></textarea>
                         </div>
 
                         <div className="mb-4">
@@ -90,7 +98,7 @@ export default function Payment() {
                         </div>
 
                         <div className="flex justify-center">
-                            <button onClick={handleSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" >Submit</button>
+                            <button  type='submit' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" >Submit</button>
                         </div>
                     </form>
                 </div>
--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -22,7 +22,7 @@ export default function CheckOut() {
                 {/* left bar */}
                 <div className='flex-grow' >
                     <div className="banner">
-                        <img className='w-full h-80' src="https://cdn.colorexpertsbd.com/wp-content/uploads/2020/11/Banner-01-01-01.jpg" alt="" />
+                        <img className='w-full h-80' src="https://t4.ftcdn.net/jpg/03/06/69/49/360_F_306694930_S3Z8H9Qk1MN79ZUe7bEWqTFuonRZdemw.jpg" alt="" />
                         <div className='bg-slate-200 m-1 p-2 '>
                             <p className='font-bold font-serif '>* {`${basketItem.length == 0 ? 'Your Basket is empty' : 'Your Items'}`}</p>
                             <hr />
@@ -36,15 +36,15 @@ export default function CheckOut() {
                     </div>
                 </div>
                 {/* Right Bar */}
-                <div className='bg-slate-200 mt-10 shadow-md relative bottm-0 shadow-black flex-grow flex-shrink lg:w-fit lg:mx-3 lg:mt-0'>
-                 { basketItem.length!=0 &&  <div className=' p-2 lg:sticky lg:top-0'>
+               {basketItem.length!=0 &&  <div className='bg-slate-200 mt-10 shadow-md relative bottm-0 shadow-black flex-grow flex-shrink lg:w-fit lg:mx-3 lg:mt-0'>
+                  <div className=' p-2 lg:sticky lg:top-0'>
                         <p className='p-2 font-bold text-black  lg:mt-0 '>Your cart: <span className='text-green-600'>{Math.floor(getPrice)}$</span> </p>
                         <hr ></hr>
                         <button className='bg-pink-500 w-full  text-white rounded-md font-serif font-bold focus:bg-orange-300  hover:bg-orange-400  lg:mt-0' onClick={redirect}>CheckOut</button>
-                    </div>}
+                    </div>
 
 
-                </div>
+                </div>}
 
             </div>
         </div>
